400c8d136eb22453465941cd2b6d97b2da3682d2
chizg <m13051839210@163.com>
2024-03-29 17:31:52 +0800

    0.1

--- chizg_tool/src/test/java/com/chizg/file/ReadFileTest.java
+++ chizg_tool/src/test/java/com/chizg/file/ReadFileTest.java
@@ -143,6 +143,67 @@ public class ReadFileTest {
     }
 
     @Test
+    public void readData20240322ProfitDayUserPPSAndPPLNS() {
+//        String sql = "UPDATE `yp_pool_user_profit_day` SET `avg_share_accept` = `avg_share_accept` %s, `day_profit` = `day_profit` %s , `total_profit` = `total_profit` %s  WHERE  `user_id` = %s and `day` = 1710979200;";
+        String sql = "UPDATE `yp_pool_user_profit_day` SET `pps_day_profit` = `pps_day_profit` %s, `pplns_day_profit` = `pplns_day_profit` %s , " +
+                "`pps_day_pool_real_profit` = `pps_day_pool_real_profit` %s,`pplns_day_pool_profit`=`pplns_day_pool_profit` %s  WHERE  `user_id` = %s and `day` = 1710979200;";
+
+        BigDecimal sumPplns = BigDecimal.ZERO ;
+        BigDecimal sumPps = BigDecimal.ZERO ;
+        BigDecimal sumPoolPps = BigDecimal.ZERO ;
+        BigDecimal sumPoolPplns = BigDecimal.ZERO ;
+        // 创建 reader
+        try (BufferedReader br = Files.newBufferedReader(Paths.get("E:\\develop\\github\\chizhigang\\data\\userPPSandPPLNS.csv"))) {
+            // CSV文件的分隔符
+            String DELIMITER = ",";
+            // 按行读取
+            String line = br.readLine();
+            while ((line = br.readLine()) != null) {
+                // 分割
+                String[] columns = line.split(DELIMITER);
+                // user_id,pps,pplns,pool-pps,pool-pplns
+                String pps = new BigDecimal(columns[1].replace("`","")).setScale(20,RoundingMode.DOWN).toPlainString();
+                String pplns = new BigDecimal(columns[2].replace("`","")).setScale(20, RoundingMode.DOWN).toPlainString();
+                String poolPps = new BigDecimal(columns[3].replace("`","")).setScale(20, RoundingMode.DOWN).toPlainString();
+                String poolPplns = new BigDecimal(columns[4].replace("`","")).setScale(20, RoundingMode.DOWN).toPlainString();
+                String userId = columns[0];
+                sumPps = sumPps.add(new BigDecimal(pps));
+                sumPplns = sumPplns.add(new BigDecimal(pplns));
+                sumPoolPps = sumPoolPps.add(new BigDecimal(poolPps));
+                sumPoolPplns = sumPoolPplns.add(new BigDecimal(poolPplns));
+                if (pplns.indexOf("-") == 0) {
+                    pplns =pplns;
+                } else {
+                    pplns = "+" + pplns;
+                }
+                if (pps.indexOf("-") == 0) {
+                    pps =pps;
+                } else {
+                    pps = "+" + pps;
+                }
+                if (poolPps.indexOf("-") == 0) {
+                    poolPps =poolPps;
+                } else {
+                    poolPps = "+" + poolPps;
+                }
+                if (poolPplns.indexOf("-") == 0) {
+                    poolPplns =poolPplns;
+                } else {
+                    poolPplns = "+" + poolPplns;
+                }
+                String format = String.format(sql,pps, pplns, poolPps,poolPplns, userId);
+                System.out.println(format);
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        System.out.println("sumPps="+sumPps.toPlainString());
+        System.out.println("sumPplns="+sumPplns.toPlainString());
+        System.out.println("sumPoolPps="+sumPoolPps.toPlainString());
+        System.out.println("sumPoolPplns="+sumPoolPplns.toPlainString());
+    }
+
+    @Test
     public void aa(){
         System.out.println( "456-asfa".indexOf("-"));
     }
