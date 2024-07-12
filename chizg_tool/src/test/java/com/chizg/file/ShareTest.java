0daac0e29e2e9abda2e7208c9eefe4abb970527d
chizg <m13051839210@163.com>
2024-07-12 19:29:23 +0800

    aa

---
+++ chizg_tool/src/test/java/com/chizg/file/ShareTest.java
@@ -0,0 +1,24 @@
+package com.chizg.file;
+
+import org.apache.commons.io.FileUtils;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.Iterator;
+
+
+public class ShareTest {
+
+//    private static final String share_format = "524288:312500000:79495195323031.48437500000000000000:aaa.123:66901663-7c20d2c6-fd45000000000000-6a7f6d58:1720718999999";
+    private static final String share_format = "524288:312500000:79495195323031.48437500000000000000:%s.%s:%s";
+
+    @Test
+    public void aa(){
+        String.s
+    }
+}
